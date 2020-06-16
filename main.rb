20ebf7ed45f29d06f9c50d8330837277eb7dcfb3
Anna Joe <57370408+Annajoe96@users.noreply.github.com>
2020-06-16 14:12:29 +0100

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,14 @@
+require './employee.rb'
+require './organization.rb'
+
+#creating a new object
+company = Organization.new 
+
+
+#adding employees to the object company
+company.add_employee(10, "anna", -1)
+company.add_employee(7, "daniel", 10)
+company.add_employee(3, "lawana", 10)
+company.add_employee(34,"lissetee", 3)
+
+
